aaea0e2542344dc5e7c1e69cf123ab6691fd9789
Issa laye KANE <issa.k@deally.fr>
2024-12-20 17:01:45 +0000

    version 1

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ app.get('*', (req, res) => res.sendFile(path.resolve('dist/cds-front/index.html'
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`App running  on: http://localhost:${port}`));
+
+
+
