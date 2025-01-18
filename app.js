05a2d27553cf62efc9d7b01177985a2b0f34468f
Issa laye KANE <issa.k@deally.fr>
2025-01-18 22:54:50 +0000

    add news version

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const port = 5751;
 app.use(express.static(__dirname + '/dist/cds-front'));
 
 app.get('*', (req, res) => res.sendFile(path.resolve('dist/cds-front/index.html')));
-
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`App running  on: http://localhost:${port}`));
